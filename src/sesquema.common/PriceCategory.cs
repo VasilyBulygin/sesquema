40ff989f31a9a5509970d5d440a04917c2ae1a25
Vasily Bulygin <vb.samara@gmail.com>
2022-01-17 16:51:28 +0400

    Added price category enumeration

---
+++ src/sesquema.common/PriceCategory.cs
@@ -0,0 +1,182 @@
+﻿// ReSharper disable InconsistentNaming
+namespace sesquema.common
+{
+    /// <summary>
+    /// Possible values that can be used to specify a price_category for item.
+    /// </summary>
+    public enum PriceCategory
+    {
+        /// <summary>
+        /// $0.25 USD
+        /// </summary>
+        VLV25,
+        /// <summary>
+        /// $0.49 USD
+        /// </summary>
+        VLV50,
+        /// <summary>
+        /// $0.75 USD
+        /// </summary>
+        VLV75,
+        /// <summary>
+        /// $0.99 USD
+        /// </summary>
+        VLV100,
+        /// <summary>
+        /// $1.49 USD
+        /// </summary>
+        VLV150,
+        /// <summary>
+        /// $1.99 USD
+        /// </summary>
+        VLV200,
+        /// <summary>
+        /// $2.49 USD
+        /// </summary>
+        VLV250,
+        /// <summary>
+        /// $2.99 USD
+        /// </summary>
+        VLV300,
+        /// <summary>
+        /// $3.49 USD
+        /// </summary>
+        VLV350,
+        /// <summary>
+        /// $3.99 USD
+        /// </summary>
+        VLV400,
+        /// <summary>
+        /// $4.49 USD
+        /// </summary>
+        VLV450,
+        /// <summary>
+        /// $4.99 USD
+        /// </summary>
+        VLV500,
+        /// <summary>
+        /// $5.49 USD
+        /// </summary>
+        VLV550,
+        /// <summary>
+        /// $5.99 USD
+        /// </summary>
+        VLV600,
+        /// <summary>
+        /// $6.49 USD
+        /// </summary>
+        VLV650,
+        /// <summary>
+        /// $6.99 USD
+        /// </summary>
+        VLV700,
+        /// <summary>
+        ///	$7.49 USD
+        /// </summary>
+        VLV750,
+        /// <summary>
+        ///	$7.99 USD
+        /// </summary>
+        VLV800,
+        /// <summary>
+        ///	$8.49 USD
+        /// </summary>
+        VLV850,
+        /// <summary>
+        ///	$8.99 USD
+        /// </summary>
+        VLV900,
+        /// <summary>
+        ///	$9.49 USD
+        /// </summary>
+        VLV950,
+        /// <summary>
+        /// $9.99 USD
+        /// </summary>
+        VLV1000,
+        /// <summary>
+        ///	$10.99 USD
+        /// </summary>
+        VLV1100,
+        /// <summary>
+        ///	$11.99 USD
+        /// </summary>
+        VLV1200,
+        /// <summary>
+        ///	$12.99 USD
+        /// </summary>
+        VLV1300,
+        /// <summary>
+        /// $13.99 USD
+        /// </summary>
+        VLV1400,
+        /// <summary>
+        ///	$14.99 USD
+        /// </summary>
+        VLV1500,
+        /// <summary>
+        ///	$15.99 USD
+        /// </summary>
+        VLV1600,
+        /// <summary>
+        ///	$16.99 USD
+        /// </summary>
+        VLV1700,
+        /// <summary>
+        ///	$17.99 USD
+        /// </summary>
+        VLV1800,
+        /// <summary>
+        ///	$18.99 USD
+        /// </summary>
+        VLV1900,
+        /// <summary>
+        ///	$19.99 USD
+        /// </summary>
+        VLV2000,
+        /// <summary>
+        ///	$24.99 USD
+        /// </summary>
+        VLV2500,
+        /// <summary>
+        ///	$29.99 USD
+        /// </summary>
+        VLV3000,
+        /// <summary>
+        ///	$34.99 USD
+        /// </summary>
+        VLV3500,
+        /// <summary>
+        ///	$39.99 USD
+        /// </summary>
+        VLV4000,
+        /// <summary>
+        ///	$44.99 USD
+        /// </summary>
+        VLV4500,
+        /// <summary>
+        ///	$49.99 USD
+        /// </summary>
+        VLV5000,
+        /// <summary>
+        ///	$59.99 USD
+        /// </summary>
+        VLV6000,
+        /// <summary>
+        ///	$69.99 USD
+        /// </summary>
+        VLV7000,
+        /// <summary>
+        ///	$79.99 USD
+        /// </summary>
+        VLV8000,
+        /// <summary>
+        ///	$89.99 USD
+        /// </summary>
+        VLV9000,
+        /// <summary>
+        ///	$99.99 USD
+        /// </summary>
+        VLV10000
+    }
+}
