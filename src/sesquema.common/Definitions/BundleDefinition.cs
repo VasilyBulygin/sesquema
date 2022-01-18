40db4677b10c936e592cab02368af73263ce0118
Vasily Bulygin <vb.samara@gmail.com>
2022-01-18 21:21:33 +0400

    Added Bundle property to the BundleDefinition class

--- src/sesquema.common/Definitions/BundleDefinition.cs
+++ src/sesquema.common/Definitions/BundleDefinition.cs
@@ -1,9 +1,17 @@
-﻿namespace sesquema.common.Definitions
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace sesquema.common.Definitions
 {
     /// <summary>
     /// Represents a collection of ItemDefs, with an associated quantity of each type. When this item is granted, it automatically expands into the set of items configured in the bundle property.
     /// </summary>
     public class BundleDefinition : DefinitionBase
     {
+        /// <summary>
+        /// Type and quantity of the contained bundle items
+        /// </summary>
+        [JsonProperty("bundle")]
+        public List<BundleRecipe> BundleRecipes { get; set; }
     }
 }
