c57deef0f75b10d540704688a31b8ed847d70fa5
Vasily Bulygin <vb.samara@gmail.com>
2022-01-18 18:40:32 +0400

    Added WorpshopId property to the ItemDefinition class

--- src/sesquema.common/Definitions/ItemDefinition.cs
+++ src/sesquema.common/Definitions/ItemDefinition.cs
@@ -1,9 +1,16 @@
-﻿namespace sesquema.common.Definitions
+﻿using Newtonsoft.Json;
+
+namespace sesquema.common.Definitions
 {
     /// <summary>
     /// An item type that can be found in a player inventory.
     /// </summary>
     public class ItemDefinition : DefinitionBase
     {
+        /// <summary>
+        /// Id to the linked workshop item
+        /// </summary>
+        [JsonProperty("workshopid")]
+        public int WorkshopId { get; set; }
     }
 }
