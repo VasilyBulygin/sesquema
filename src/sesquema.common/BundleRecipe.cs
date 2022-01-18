2815892fc8763919d3e31d4828574279686801c0
Vasily Bulygin <vb.samara@gmail.com>
2022-01-18 21:13:31 +0400

    Added BundleRecipe class

---
+++ src/sesquema.common/BundleRecipe.cs
@@ -0,0 +1,18 @@
+﻿namespace sesquema.common
+{
+    /// <summary>
+    /// Bundle item recipe
+    /// </summary>
+    public class BundleRecipe
+    {
+        /// <summary>
+        /// Id of item definition.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Desired quantity.
+        /// </summary>
+        public uint Quantity { get; set; }
+    }
+}
