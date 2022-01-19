76b10923b07cebfe6829b974cc43baa9353fbba9
Vasily Bulygin <vb.samara@gmail.com>
2022-01-19 18:55:30 +0400

    Added Schema class

---
+++ src/sesquema.common/Schema.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using sesquema.common.Definitions;
+
+namespace sesquema.common
+{
+    public class Schema
+    {
+        [JsonProperty("appid")]
+        public uint AppId { get; set; }
+
+        [JsonProperty("items")]
+        public List<DefinitionBase> Definitions { get; set; }
+    }
+}
