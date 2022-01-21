99d48d27105347d03315ff233d52204f0a52723a
Vasily Bulygin <vb.samara@gmail.com>
2022-01-21 21:59:07 +0400

    Removed unnecessary class

--- src/sesquema.protocol/Schema.cs
+++ src/sesquema.protocol/Schema.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using sesquema.protocol.Definitions;
 
 namespace sesquema.protocol
@@ -21,23 +19,6 @@ namespace sesquema.protocol
         /// List of all item definitions.
         /// </summary>
         [JsonProperty("items")]
-        public List<DefinitionBase> Definitions { get; set; }
-    }
-
-    public class DefinitionConverter : JsonConverter
-    {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            return null;
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return true;
-        }
+        public List<DefinitionBase> Definitions { get; set; } = new();
     }
 }
