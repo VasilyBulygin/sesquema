30e31227797d5853551ee9115aba5170e3e4a42b
Vasily Bulygin <vb.samara@gmail.com>
2022-01-22 18:50:37 +0400

    Removed LocalizablePropertyConverter

--- src/sesquema.protocol/SchemaSerializer.cs
+++ src/sesquema.protocol/SchemaSerializer.cs
@@ -78,22 +78,4 @@ namespace sesquema.protocol
             }
         }
     }
-
-    internal class LocalizablePropertyConverter : JsonConverter
-    {
-        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
