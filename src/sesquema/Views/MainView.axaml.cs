223e4b343553772406a7d0ebf05b40b8385a5c10
Vasily Bulygin <vb.samara@gmail.com>
2022-01-26 20:31:24 +0400

    Renamed MainWindow to MainView

--- src/sesquema/Views/MainView.axaml.cs
+++ src/sesquema/Views/MainView.axaml.cs
@@ -2,11 +2,11 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace sesquema
+namespace sesquema.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
 #if DEBUG
