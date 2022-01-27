c99b1431135bc4c656ab3d8f4f650d57ba42d7b1
Vasily Bulygin <vb.samara@gmail.com>
2022-01-27 21:40:47 +0400

    Update MainView.axaml.cs

--- src/sesquema/Views/MainView.axaml.cs
+++ src/sesquema/Views/MainView.axaml.cs
@@ -1,18 +1,27 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using sesquema.ViewModels;
 
 namespace sesquema.Views
 {
     public partial class MainView : Window
     {
-        public MainView()
+        public MainView(MainViewModel viewModel)
         {
+            ViewModel = viewModel;
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
         }
+
+        public MainView() : this(new MainViewModel())
+        {
+
+        }
+
+        public MainViewModel ViewModel { get; }
 
         private void InitializeComponent()
         {
