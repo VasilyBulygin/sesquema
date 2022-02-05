d421a4ef81b67cc3ee0c682ba0e82d2fff2b34c2
Vasily Bulygin <vb.samara@gmail.com>
2022-02-05 22:12:00 +0400

    Update ItemDefinitionViewModel.cs

--- src/sesquema/ViewModels/ItemDefinitionViewModel.cs
+++ src/sesquema/ViewModels/ItemDefinitionViewModel.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using sesquema.protocol.Definitions;
 
 namespace sesquema.ViewModels
 {
-    public class ItemDefinitionViewModel : BaseViewModel
+    public class ItemDefinitionViewModel : BaseViewModel<ItemDefinition>
     {
+        public ItemDefinitionViewModel(ItemDefinition model) : base(model)
+        {
+        }
     }
 }
