4a74f56e3a70b1234323edfad63e2419f9a63e35
Vasily Bulygin <vb.samara@gmail.com>
2022-02-15 22:27:45 +0400

    Create PlaytimeGeneratorItemDefinitionViewModel.cs

---
+++ src/sesquema/ViewModels/PlaytimeGeneratorItemDefinitionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using sesquema.protocol.Definitions;
+
+namespace sesquema.ViewModels
+{
+    public class PlaytimeGeneratorItemDefinitionViewModel : ItemDefinitionViewModel
+    {
+        public PlaytimeGeneratorItemDefinitionViewModel(ItemDefinition model) : base(model)
+        {
+        }
+    }
+}
