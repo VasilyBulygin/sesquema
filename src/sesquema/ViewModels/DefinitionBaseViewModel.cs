841ae7ff3a3a990f51644bfe16fa9499ed9b7e40
Vasily Bulygin <vb.samara@gmail.com>
2022-02-15 22:30:19 +0400

    Create DefinitionBaseViewModel.cs

---
+++ src/sesquema/ViewModels/DefinitionBaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using sesquema.protocol.Definitions;
+
+namespace sesquema.ViewModels
+{
+    public abstract class DefinitionBaseViewModel<T> : BaseViewModel<T> where T : DefinitionBase
+    {
+        protected DefinitionBaseViewModel(T model) : base(model)
+        {
+        }
+    }
+}
