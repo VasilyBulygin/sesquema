7989e01c78709ad5914d87265021d37510e49a41
Vasily Bulygin <vb.samara@gmail.com>
2022-02-16 22:30:27 +0400

    Renamed PlaytimeGeneratorDefinitionViewModel

---
+++ src/sesquema/ViewModels/PlaytimeGeneratorDefinitionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using sesquema.protocol.Definitions;
+
+namespace sesquema.ViewModels
+{
+    public class PlaytimeGeneratorDefinitionViewModel : DefinitionBaseViewModel<PlaytimeGeneratorDefinition>
+    {
+        public PlaytimeGeneratorDefinitionViewModel(PlaytimeGeneratorDefinition model) : base(model)
+        {
+        }
+    }
+}
