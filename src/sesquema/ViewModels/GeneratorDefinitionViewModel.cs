06bdadcf68d6634b97bcd05a417326c87fa96c98
Vasily Bulygin <vb.samara@gmail.com>
2022-02-16 22:30:03 +0400

    Create GeneratorDefinitionViewModel.cs

---
+++ src/sesquema/ViewModels/GeneratorDefinitionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using sesquema.protocol.Definitions;
+
+namespace sesquema.ViewModels
+{
+    public class GeneratorDefinitionViewModel : DefinitionBaseViewModel<GeneratorDefinition>
+    {
+        public GeneratorDefinitionViewModel([NotNull] GeneratorDefinition model) : base(model)
+        {
+        }
+    }
+}
