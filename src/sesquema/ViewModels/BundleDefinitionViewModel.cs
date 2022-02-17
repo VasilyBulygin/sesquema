bc0dccdc680bd04a03ca39e93542f2fa982f1316
Vasily Bulygin <vb.samara@gmail.com>
2022-02-17 22:33:25 +0400

    Create BundleDefinitionViewModel.cs

---
+++ src/sesquema/ViewModels/BundleDefinitionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using sesquema.protocol.Definitions;
+
+namespace sesquema.ViewModels
+{
+    public class BundleDefinitionViewModel : DefinitionBaseViewModel<BundleDefinition>
+    {
+        public BundleDefinitionViewModel(BundleDefinition model) : base(model)
+        {
+        }
+    }
+}
